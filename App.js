bb4efd87ec470cf6e34c151708b4b52129acfd2e
Sherange <sherangef@gmail.com>
2022-08-27 15:30:12 +0530

    add UI component with unit test

--- App.js
+++ App.js
@@ -7,14 +7,24 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import AppLoader from './src/components/AppLoader/AppLoader';
+import {backgroundColor} from './src/constans/theme';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle={'dark-content'} />
+      <AppLoader />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: backgroundColor,
+  },
+});
 
 export default App;
